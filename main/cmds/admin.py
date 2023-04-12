36c979b9a3bd68fd8ef2741d5102a14291f557ad
abraker95 <abraker95@gmail.com>
2023-04-11 20:47:32 -0400

    refactor outdated way of check when sending

--- main/cmds/admin.py
+++ main/cmds/admin.py
@@ -17,9 +17,7 @@ class CmdsAdmin:
             return
 
         status = discord.Embed(title=':skull_crossbones: Sigma Shutting Down.', color=0x808080)
-        
-        try: await msg.channel.send(None, embed=status)
-        except: pass
+        await msg.channel.send(None, embed=status)
     
         # Shutdown the discord client
         await self.close()
