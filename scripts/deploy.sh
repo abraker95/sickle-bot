5b79561047314d05e7b87d6e830b27596802db36
abraker95 <abraker95@gmail.com>
2023-07-03 15:05:03 -0400

    fix config.py being nuked

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,9 @@ $(dirname ${0%})/build.sh
 #
 # To be run as root
 systemctl stop sickle.service
+mv /home/server/prod/sickle-bot/config.py /home/server/tmp/config.py
 rm -rf /home/server/prod/sickle-bot
 rsync -av --progress . /home/server/prod/sickle-bot --exclude config.py
 chown -R server:server /home/server/prod/sickle-bot
+mv /home/server/tmp/config.py /home/server/prod/sickle-bot/config.py
 systemctl start sickle.service
