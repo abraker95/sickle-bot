8b4a516b489025c9238d8761c9de82ded6ed1b52
abraker95 <abraker95@gmail.com>
2023-07-03 15:52:16 -0400

    Fix db being nuked at deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,10 @@ $(dirname ${0%})/build.sh
 # To be run as root
 systemctl stop sickle.service
 mv /home/server/prod/sickle-bot/config.py /home/server/tmp/config.py
+mv /home/server/prod/sickle-bot/db.json /home/server/tmp/db.json
 rm -rf /home/server/prod/sickle-bot
 rsync -av --progress . /home/server/prod/sickle-bot --exclude config.py
 chown -R server:server /home/server/prod/sickle-bot
 mv /home/server/tmp/config.py /home/server/prod/sickle-bot/config.py
+mv /home/server/tmp/db.json /home/server/prod/sickle-bot/db.json
 systemctl start sickle.service
