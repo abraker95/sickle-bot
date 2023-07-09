65060fbd75d87de3ae5ed020cac88d339cf9008c
abraker95 <abraker95@gmail.com>
2023-07-09 19:02:47 -0400

    improve deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,24 @@ $(dirname ${0%})/build.sh
 # that edits can be made in this dev location
 #
 # To be run as root
+su root
+
 systemctl stop sickle.service
+
 mv /home/server/prod/sickle-bot/config.py /home/server/tmp/config.py
 mv /home/server/prod/sickle-bot/db.json /home/server/tmp/db.json
+
 rm -rf /home/server/prod/sickle-bot
 rsync -av --progress . /home/server/prod/sickle-bot --exclude config.py
 chown -R server:server /home/server/prod/sickle-bot
+
 mv /home/server/tmp/config.py /home/server/prod/sickle-bot/config.py
 mv /home/server/tmp/db.json /home/server/prod/sickle-bot/db.json
+
+# Sensitive files should only be accesible by the user these files are for
+chown root:server /home/server/prod/sickle-bot/db.json
+chmod 640 /home/server/prod/sickle-bot/db.json
+chown root:server /home/server/prod/sickle-bot/db.json
+chmod 660 /home/server/prod/sickle-bot/db.json
+
 systemctl start sickle.service
